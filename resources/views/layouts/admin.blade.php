4774f8ca22eea72a2038b066837fddd68d377f10
Nathan Burgess <nburgess@cantongroup.com>
2015-04-08 15:51:31 -0400

    Added TODOs for changing i to span

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,7 +24,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">L5Boot</a>
+            <a class="navbar-brand" href="{{ route('admin') }}">L5Boot</a>
         </div>
 
         <ul class="nav navbar-top-links navbar-right">
@@ -223,13 +223,10 @@
                     <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                 </a>
                 <ul class="dropdown-menu dropdown-user">
-                    <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                    </li>
-                    <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                    </li>
+                    <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a></li>
+                    <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a></li>
                     <li class="divider"></li>
-                    <li><a href="#"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                    </li>
+                    <li><a href="#"><i class="fa fa-sign-out fa-fw"></i> Logout</a></li>
                 </ul>
                 <!-- /.dropdown-user -->
             </li>
@@ -240,62 +237,29 @@
     <nav class="navbar-default navbar-side" role="navigation">
         <div class="sidebar-collapse">
             <ul class="nav" id="main-menu">
-
-                <li>
-                    <a class="active-menu" href="index.html"><i class="fa fa-dashboard"></i> Dashboard</a>
-                </li>
-                <li>
-                    <a href="ui-elements.html"><i class="fa fa-desktop"></i> UI Elements</a>
-                </li>
-                <li>
-                    <a href="chart.html"><i class="fa fa-bar-chart-o"></i> Charts</a>
-                </li>
-                <li>
-                    <a href="tab-panel.html"><i class="fa fa-qrcode"></i> Tabs & Panels</a>
-                </li>
-
-                <li>
-                    <a href="table.html"><i class="fa fa-table"></i> Responsive Tables</a>
-                </li>
-                <li>
-                    <a href="form.html"><i class="fa fa-edit"></i> Forms </a>
-                </li>
-
-
+                <li><a class="active-menu" href="#!"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                <li><a href="#!"><i class="fa fa-desktop"></i> UI Elements</a></li>
+                <li><a href="#!"><i class="fa fa-bar-chart-o"></i> Charts</a></li>
+                <li><a href="#!"><i class="fa fa-qrcode"></i> Tabs & Panels</a></li>
+                <li><a href="#!"><i class="fa fa-table"></i> Responsive Tables</a></li>
+                <li><a href="#!"><i class="fa fa-edit"></i> Forms </a></li>
                 <li>
                     <a href="#"><i class="fa fa-sitemap"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level">
-                        <li>
-                            <a href="#">Second Level Link</a>
-                        </li>
-                        <li>
-                            <a href="#">Second Level Link</a>
-                        </li>
-                        <li>
-                            <a href="#">Second Level Link<span class="fa arrow"></span></a>
+                        <li><a href="#">Second Level Link</a></li>
+                        <li><a href="#">Second Level Link</a></li>
+                        <li><a href="#">Second Level Link<span class="fa arrow"></span></a>
                             <ul class="nav nav-third-level">
-                                <li>
-                                    <a href="#">Third Level Link</a>
-                                </li>
-                                <li>
-                                    <a href="#">Third Level Link</a>
-                                </li>
-                                <li>
-                                    <a href="#">Third Level Link</a>
-                                </li>
-
+                                <li><a href="#">Third Level Link</a></li>
+                                <li><a href="#">Third Level Link</a></li>
+                                <li><a href="#">Third Level Link</a></li>
                             </ul>
-
                         </li>
                     </ul>
                 </li>
-                <li>
-                    <a href="empty.html"><i class="fa fa-fw fa-file"></i> Empty Page</a>
-                </li>
+                <li><a href="#!"><i class="fa fa-fw fa-file"></i> Empty Page</a></li>
             </ul>
-
         </div>
-
     </nav>
     <!-- /. NAV SIDE  -->
     <div id="page-wrapper">
